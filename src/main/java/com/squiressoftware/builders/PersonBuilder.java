bb8dac8b84ef2ade2620da64c8754a14bd592663
Tom Squires <mjollnir8@gmail.com>
2015-04-10 13:20:47 +0100

    make getCount work. Impliment person builder

--- src/main/java/com/squiressoftware/builders/PersonBuilder.java
+++ src/main/java/com/squiressoftware/builders/PersonBuilder.java
@@ -1,13 +1,27 @@
 package com.squiressoftware.builders;
 
 import com.squiressoftware.entities.Person;
+import com.squiressoftware.enums.Sex;
 
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class PersonBuilder {
 
-	public static Person buildFromCSVRow(String data){
-		throw new NotImplementedException();
+	public Person buildFromCSVRow(String data) {
+		String[] fields = data.split(",");
+		String fullName = fields[0].trim();
+		Sex sex = Sex.valueOf(fields[1].trim().toUpperCase());
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
+		LocalDate dob = LocalDate.parse(fields[2].trim(), formatter);
+
+		//Person probably wasn't born in the future. Assume it was decade before
+		if (dob.isAfter(LocalDate.now())) {
+			dob.minus(1, ChronoUnit.CENTURIES);
+		}
+
+		return new Person(fullName, sex, dob);
 	}
 }
--- src/main/java/com/squiressoftware/entities/AddressBookImpl.java
+++ src/main/java/com/squiressoftware/entities/AddressBookImpl.java
@@ -1,5 +1,6 @@
 package com.squiressoftware.entities;
 
+import com.squiressoftware.builders.PersonBuilder;
 import com.squiressoftware.enums.Sex;
 
 
@@ -8,23 +9,24 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class AddressBookImpl implements AddressBook {
 
-	public AddressBookImpl(String dataLocation) throws IOException, URISyntaxException {
-		String addressData = getFileFromClassPath("AddressBook");
-		String[] rows = addressData.split(System.lineSeparator());
-	}
+	private PersonBuilder personBuilder = new PersonBuilder();
+	private Set<Person> people = new HashSet<>();
 
-	private String getFileFromClassPath(String fileName) throws IOException, URISyntaxException {
-		Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
-		return new String(Files.readAllBytes(path), "UTF-8");
+	public AddressBookImpl(String dataLocation) throws IOException, URISyntaxException {
+		String addressData = getFileFromClassPath(dataLocation);
+		parseAndAddToPeopleList(addressData);
 	}
 
 	@Override
 	public long getCount(Sex sex) {
-		return 0;
+		return people.stream().filter(person -> person.getSex() == sex).count();
 	}
 
 	@Override
@@ -41,4 +43,15 @@ public class AddressBookImpl implements AddressBook {
 	public List<Person> getAll() {
 		return null;
 	}
+
+	private void parseAndAddToPeopleList(String addressData) {
+		String[] rows = addressData.split(System.lineSeparator());
+		Arrays.asList(rows).forEach(row -> people.add(personBuilder.buildFromCSVRow(row)));
+	}
+
+	private String getFileFromClassPath(String fileName) throws IOException, URISyntaxException {
+		Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+		return new String(Files.readAllBytes(path), "UTF-8");
+	}
+
 }
